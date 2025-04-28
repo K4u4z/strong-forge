fcdccac478fa6b634687b7831a1431b28260e796
K4U4Z <kz.dio7@outlook.com>
2025-04-27 23:43:46 -0300

    Enviando alterações com implementação dos metodos e da classe exercicio.java

---
+++ Projeto/src/main/java/br/com/strongforge/dao/ExercicioDao.java
@@ -0,0 +1,137 @@
+package br.com.strongforge.dao;
+
+import br.com.strongforge.config.ConnectionPoolConfig;
+import br.com.strongforge.model.Exercicio;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ExercicioDao {
+    public void createExercicio(Exercicio exercicio){
+        String SQL = "INSERT INTO EXERCICIO (NOME,AGRUPAMENTO,NIVEL,DESCRICAO,IMAGE,VIDEO) VALUES (?,?,?,?,?,?)";
+
+        try{
+            Connection connection = ConnectionPoolConfig.getConnection();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1,exercicio.getNome());
+            preparedStatement.setString(2, exercicio.getAgrupamento());
+            preparedStatement.setString(3, exercicio.getNivel());
+            preparedStatement.setString(4, exercicio.getDescricao());
+            preparedStatement.setString(5, exercicio.getImage());
+            preparedStatement.setString(6, exercicio.getVideo());
+            preparedStatement.execute();
+
+            System.out.println("Parâmetro inserido com sucesso");
+
+            connection.close();
+
+        }catch(Exception e){
+            System.out.println("Erro na conexão do banco");
+            System.out.println("Error"+ e.getMessage());
+        }
+
+
+    }
+
+    public List<Exercicio> findAllExercicio() {
+        String SQL = "SELECT * FROM EXERCICIO";
+
+
+        try {
+
+            Connection connection = ConnectionPoolConfig.getConnection();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Exercicio> exercicios = new ArrayList<>();
+
+            while (resultSet.next()) {
+
+                String exercicioId = resultSet.getString("id");
+                String exercicioName = resultSet.getString("nome");
+                String exercicioAgrup = resultSet.getString("agrupamento");
+                String exercicioNivel = resultSet.getString("nivel");
+                String exercicioDescricao = resultSet.getString("descricao");
+                String exercicioImage = resultSet.getString("image");
+                String exercicioVideo = resultSet.getString("video");
+
+                Exercicio exercicio = new Exercicio(exercicioId,exercicioName,exercicioAgrup,exercicioNivel,exercicioDescricao,exercicioImage,exercicioVideo);
+
+                exercicios.add(exercicio);
+
+            }
+
+            System.out.println("sucesso ao selecionar * exercicio");
+
+            connection.close();
+
+            return exercicios;
+
+        } catch (Exception e) {
+
+            System.out.println("Falha ao se conectar com o banco");
+            System.out.println("Error" + e.getMessage());
+
+            return Collections.emptyList();
+
+        }
+
+    }
+    public void updateExercicio(Exercicio exercicio){
+        String SQL = "UPDATE EXERCICIO SET NOME = ? WHERE ID = ?";
+
+        try {
+
+            Connection connection = ConnectionPoolConfig.getConnection();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setString(1, exercicio.getNome());
+            preparedStatement.setString(2, exercicio.getId());
+            preparedStatement.execute();
+
+            System.out.println("success em atualizar o exercicio");
+
+            connection.close();
+
+        } catch (Exception e) {
+
+            System.out.println("falha na conexão com o banco");
+            System.out.println("Error: " + e.getMessage());
+
+        }
+
+
+    }
+
+    public void deleteExercicioById(String exercicioId){
+        String SQL = "DELETE EXERCICIO WHERE ID = ?";
+
+
+        try {
+
+            Connection connection = ConnectionPoolConfig.getConnection();
+
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1, exercicioId);
+            preparedStatement.execute();
+
+            System.out.println("success ao deletar o exercicio pelo ID " + exercicioId);
+
+            connection.close();
+
+        } catch (Exception e) {
+
+            System.out.println("falha ao conectar com o banco");
+
+        }
+    }
+}
---
+++ Projeto/src/main/java/br/com/strongforge/servlet/ListExercicioServlet.java
@@ -0,0 +1,27 @@
+package br.com.strongforge.servlet;
+
+import br.com.strongforge.dao.ExercicioDao;
+import br.com.strongforge.dao.UsuarioDao;
+import br.com.strongforge.model.Exercicio;
+import br.com.strongforge.model.Usuario;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet({"/findAllExercicio", "/admin/findAllExercicio"})
+public class ListExercicioServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Exercicio> exercicios = new ExercicioDao().findAllExercicio();
+
+        req.setAttribute("exercicios", exercicios);
+
+        req.getRequestDispatcher("admin.jsp").forward(req,resp);
+    }
+}
