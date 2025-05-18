355d1d616f101523dec74c698fe3bcce8ae1ec0c
K4U4Z <kz.dio7@outlook.com>
2025-05-18 03:46:06 -0300

    Enviando alteração de criar detalhes da lista

--- Projeto/src/main/java/br/com/strongforge/dao/ListaDao.java
+++ Projeto/src/main/java/br/com/strongforge/dao/ListaDao.java
@@ -107,6 +107,67 @@ public class ListaDao {
         }
     }
 
+    public Lista detalhesListaById(String listaId) {
+        String SQL = "SELECT \n" +
+                "    L.id AS lista_id,\n" +
+                "    L.nome AS nome_lista,\n" +
+                "    E.id AS exercicio_id,\n" +
+                "    E.nome AS nome_exercicio,\n" +
+                "    E.agrupamento,\n" +
+                "    E.nivel,\n" +
+                "    E.descricao,\n" +
+                "    E.image,\n" +
+                "    E.video\n" +
+                "FROM \n" +
+                "    Lista L\n" +
+                "INNER JOIN LISTA_EXERCICIO_ITEM LE ON L.id = LE.lista_id\n" +
+                "INNER JOIN Exercicio E ON LE.exercicio_id = E.id\n" +
+                "WHERE L.id = ?";
+
+        try {
+            Connection connection = ConnectionPoolConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1, listaId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            Lista lista = null;
+            List<Exercicio> exercicios = new ArrayList<>();
+
+            while (resultSet.next()) {
+                if (lista == null) {
+                    lista = new Lista(
+                            resultSet.getString("lista_id"),
+                            resultSet.getString("nome_lista")
+                    );
+                }
+
+                Exercicio exercicio = new Exercicio(
+                        resultSet.getString("exercicio_id"),
+                        resultSet.getString("nome_exercicio"),
+                        resultSet.getString("agrupamento"),
+                        resultSet.getString("nivel"),
+                        resultSet.getString("descricao"),
+                        resultSet.getString("image"),
+                        resultSet.getString("video")
+                );
+                exercicios.add(exercicio);
+            }
+
+            if (lista != null) {
+                lista.setExercicios(exercicios);
+            }
+
+            connection.close();
+            return lista;
+
+        } catch(Exception e) {
+            System.out.println("Falha ao se conectar com o banco");
+            System.out.println("Erro: " + e.getMessage());
+            return null;
+        }
+    }
+
     /* public void cadastraTreino() {
 
         List<Exercicio> exercicios new ArrayList<String>();
--- Projeto/src/main/java/br/com/strongforge/model/Lista.java
+++ Projeto/src/main/java/br/com/strongforge/model/Lista.java
@@ -1,14 +1,29 @@
 package br.com.strongforge.model;
 
+import java.util.List;
+
 public class Lista {
     private String id;
     private String nome;
     private String data;
+    private List<Exercicio> exercicios;
 
     public Lista(String id, String nome, String data) {
         this.id = id;
         this.nome = nome;
         this.data = data;
+    }
+    public Lista(String id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public void setExercicios(List<Exercicio> exercicios) {
+        this.exercicios = exercicios;
+    }
+
+    public List<Exercicio> getExercicios() {
+        return exercicios;
     }
 
     public String getId() {
